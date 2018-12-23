ccffdb101c0b2e81772d02b40b7be332099e81f4
Eddie Maldonado <eddie@lemald.org>
2018-12-23 16:44:23 -0500

    Actually make the client work

--- src/TAPI.hs
+++ src/TAPI.hs
@@ -3,16 +3,29 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module TAPI where
 
 import Data.Aeson
-import GHC.Generics
+import Data.Proxy
 import Data.Text
 import Data.Time (UTCTime)
+import GHC.Generics
+import Network.HTTP.Media ((//), (/:))
 import Servant.API
+import Servant.API.ContentTypes
 
-type VehicleAPI = "vehicles" :> QueryParam "api-key" Text :> QueryParam "filter[route]" RouteID :> Get '[JSON] APIResponse
+data TJSON = TJSON JSON
+
+instance FromJSON a => MimeUnrender TJSON a where
+  mimeUnrender _ = eitherDecodeLenient
+
+instance Accept TJSON where
+  contentType _ = "application" // "vnd.api+json" /: ("charset", "utf-8")
+
+type VehicleAPI = "vehicles" :> QueryParam "api-key" Text :> QueryParam "filter[route]" RouteID :> Get '[TJSON] APIResponse
 
 type RouteID = Text
 
