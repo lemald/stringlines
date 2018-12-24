a2e41f388d2b7996596f88615eb3ffa1c6337963
Eddie Maldonado <eddie@lemald.org>
2018-12-24 13:17:45 -0500

    Initial work on main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
-import Lib
+import Control.Concurrent
+import Control.Concurrent.MVar
+import Client
 
 main :: IO ()
-main = someFunc
+main = do
+  -- We'll be getting an MVar for each thread to wait on it
+  mvar <- newEmptyMVar
+  thread <- forkFinally (threadDelay 5000000 >> putStrLn "foo") (putMVar mvar)
+  takeMVar mvar
+  return ()
