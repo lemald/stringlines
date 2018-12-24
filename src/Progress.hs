aa408e15d2afc4875f0ceee97a4c82cc20a89003
Eddie Maldonado <eddie@lemald.org>
2018-12-24 11:17:55 -0500

    Initial work on computing vehicle progress

---
+++ src/Progress.hs
@@ -0,0 +1,18 @@
+module Progress where
+
+import TAPI
+import Data.Geo.Jord.Geodetics
+import Data.Geo.Jord.LatLong
+import Data.Text as T
+import GPolyline
+
+progressOnRoute :: Vehicle -> Shape -> Double
+progressOnRoute vehicle shape = let
+  vehicle_pos = (decimalLatLong (TAPI.latitude vehicle) (TAPI.longitude vehicle))
+  points = shapeToPoints shape
+  in 1.0 -- dummy double to make it typecheck while WIP
+
+shapeToPoints :: Shape -> [LatLong]
+shapeToPoints s = let
+  line = decodeline (T.unpack $ polyline s)
+  in Prelude.map (\(lat, lon) -> decimalLatLong lat lon) line
