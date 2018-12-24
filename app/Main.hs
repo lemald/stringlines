0a97324255eef6be6f02da59bafdab6a9745f203
Eddie Maldonado <eddie@lemald.org>
2018-12-24 14:31:18 -0500

    Get the main loop up and running

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Concurrent
 import Control.Concurrent.MVar
+import Control.Monad
 import Client
+import TAPI
+
+routes = ["77", "71", "73"]
 
 main :: IO ()
 main = do
-  -- We'll be getting an MVar for each thread to wait on it
-  mvar <- newEmptyMVar
-  thread <- forkFinally (threadDelay 5000000 >> putStrLn "foo") (putMVar mvar)
-  takeMVar mvar
+  mvars <- mapM (\r -> do
+                    mvar <- newEmptyMVar
+                    thread <- forkFinally (routeLoop r) (putMVar mvar)
+                    return mvar)
+           routes
+  mapM_ (\m -> takeMVar m) mvars
   return ()
+
+routeLoop :: TAPI.RouteID -> IO()
+routeLoop r = do
+  res <- queryAPI $ getVehicles r
+  putStrLn $ show res
+  threadDelay (15 * 1000 * 1000)
+  routeLoop r
+  
+  
