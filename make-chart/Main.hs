8ba97ca6a2e9dc15937b7e0ec3bea0e324ce739f
Eddie Maldonado <eddie@lemald.org>
2019-01-05 14:41:42 -0500

    Basic Gnuplot stuff

--- make-chart/Main.hs
+++ make-chart/Main.hs
@@ -1,8 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Data.Time.Calendar
+import Data.Time.LocalTime
 import Graphics.Gnuplot.Simple
 import Graphics.Gnuplot.Terminal.PNG
+import Graphics.Gnuplot.Time
+
+import Client
+import DataStore
+import TAPI
 
 main :: IO()
 main = do
-  putStrLn "Hello world!"
+  con <- connectToDB
+  -- TODO: Take this stuff in as arguments
+  results <- tripInfoByRouteForDay con "77" (read "2019-01-04" :: Day) (hoursToTimeZone (-5))
+  plotPathsStyle
+    [Key Nothing
+    ,XLabel "Time"
+    ,XTime
+    ,YLabel "Progress along route"
+    ,YRange (0, 1)
+    -- TODO: Output file should also come from args
+    ,terminal $ Graphics.Gnuplot.Terminal.PNG.cons "output.png"
+    ]
+    (resultsToPaths results)
+
+resultsToPaths :: [TripInfo] -> [(PlotStyle, [(Double, Double)])]
+resultsToPaths ts = []
